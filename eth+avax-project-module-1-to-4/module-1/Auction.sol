44b09c66345f244c8f5625eaebd88b66cddd89c0
Jerwin Peralta <jerwin_tuchi21@yahoo.com>
2023-11-23 18:09:39 +0800

    Update Auction.sol
    updated a bug where the original owner of the item's address doesn't reflect

--- eth+avax-project-module-1-to-4/module-1/Auction.sol
+++ eth+avax-project-module-1-to-4/module-1/Auction.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract Auction {
-    address public owner;
+    address public original_owner;
     address public highestBidder;
     uint256 public highestBid;
     uint256 public auctionEndTime;
@@ -20,15 +20,15 @@ contract Auction {
     event ItemTransferred(address newOwner);
 
     constructor(uint256 _duration, string memory _item, uint256 _itemId) {
-        owner = msg.sender;
-        itemOwners[owner] = _item;
-        isItemOwner[owner] = true;
+        original_owner = msg.sender;
+        itemOwners[original_owner] = _item;
+        isItemOwner[original_owner] = true;
         itemId = _itemId;
         auctionEndTime = block.timestamp + _duration;
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner of the item can finalize the bidding");
+        require(msg.sender == original_owner, "Only the owner of the item can finalize the bidding");
         _;
     }
 
@@ -45,7 +45,7 @@ contract Auction {
     modifier hasBalance(){
         require(msg.sender.balance > 0 , "Account has insufficient balance");
         _;
-    }
+    }     
 
     function getUserBalance(address _user) external view returns (uint256) {
         return userBalances[_user];
@@ -56,10 +56,11 @@ contract Auction {
     }
 
     function placeBid(uint256 _bid) external payable auctionNotEnded notItemOwner {
+        assert(_bid > 100); // assume a case where the bid amount must not be less than 100
         require(_bid > highestBid, "Bid must be higher than current highest bid");
         address previousHighestBidder = highestBidder;
         uint256 previousHighestBidAmount = highestBid;
-
+        
         if (msg.sender.balance <= _bid) {
             revert("Insufficient balance to place the bid");
         }
@@ -76,26 +77,37 @@ contract Auction {
         updateUserBalance(msg.sender, msg.sender.balance - _bid);
         currentBid[msg.sender] = _bid;
 
+
         emit PreviousBidAmount(previousHighestBid);
     }
+
+    event NoBidPlaced(string);
 
     function finalizeAuction() external onlyOwner {
         require(block.timestamp >= auctionEndTime, "Auction end time not reached");
         require(!auctionEnded, "Auction already ended");
-        auctionEnded = true;
-        require(highestBidder != address(0), "No bids placed");
 
-        transferItemToWinner();
-    }
+        // Check if no bids have been placed and the auction has ended
+        if (highestBid == 0) {
+            auctionEnded = true;
+            emit NoBidPlaced("No bid placed and bidding has expired");
+            // or revert with a custom error message
+            // revert("No bid placed and bidding has expired");
+        } else {
+            require(highestBidder != address(0), "No bids placed");
+            auctionEnded = true;
+            transferItemToWinner();
+        }
+}
 
     function transferItemToWinner() internal {
         require(itemId != 0, "No item to transfer");
         require(isItemOwner[highestBidder] == false, "Bidder already owns an item");
 
         isItemOwner[highestBidder] = true;
-        isItemOwner[owner] = false;
-        itemOwners[highestBidder] = itemOwners[owner];
-        delete itemOwners[owner];
+        isItemOwner[original_owner] = false;
+        itemOwners[highestBidder] = itemOwners[original_owner];
+        delete itemOwners[original_owner];
 
         emit ItemTransferred(highestBidder);
     }
